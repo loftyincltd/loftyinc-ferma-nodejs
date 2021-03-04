f9efad29867d50f2a1586f6b0283cd4dbd12dc47
Odekunle Oluwafemi Michael <michaelfemi81@gmail.com>
2021-03-04 23:20:30 +0100

    newt

--- python/app.py
+++ python/app.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from enhance import image_enhance
 from skimage.morphology import skeletonize, thin
 
-os.chdir("/app/")
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def removedot(invertThin):
     temp0 = numpy.array(invertThin[:])
